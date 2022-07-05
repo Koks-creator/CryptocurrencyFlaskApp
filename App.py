abb517b60ba460a9bf49ca73e3cf7c7b948054ee
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2022-07-05 22:34:36 +0200

    Update App.py

--- App.py
+++ App.py
@@ -20,15 +20,15 @@ from PIL import Image
 import os
 
 
-app = Flask(__name__, static_folder='C:/Users/Piotr/PycharmProjects/Pajton/StronkaJakas/Static')
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba244'
+app = Flask(__name__, static_folder='<static folder path>')
+app.config['SECRET_KEY'] = '<secret key>'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["MAIL_SERVER"] = 'smtp.googlemail.com'
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "tabletlenovo.an@gmail.com"
-app.config["MAIL_PASSWORD"] = ""
+app.config["MAIL_USERNAME"] = "<email>"
+app.config["MAIL_PASSWORD"] = "<password>"
 mail = Mail(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
